ab427884807e85c1dec556a34cb3b67a198a4699
glennlandman19-bit <glennlandman19@gmail.com>
2026-03-27 12:46:16 +0200

    Create jest.config.js

---
+++ backend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/tests/**/*.test.js'],
+  collectCoverageFrom: [
+    'routes/**/*.js',
+    'middleware/**/*.js',
+    '!**/node_modules/**',
+  ],
+};
